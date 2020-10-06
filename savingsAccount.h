7737948d0453a58c9eccd7c3f5a96b124847737b
jaronritter01 <61990047+jaronritter01@users.noreply.github.com>
2020-10-06 16:49:08 -0500

    Updates the gitignore file to ignore the testing files and exe; adds savings account class; updates the bank.cpp

---
+++ savingsAccount.h
@@ -0,0 +1,17 @@
+#ifndef SAVINGSACCOUNT
+#define SAVINGSACCOUNT
+#include "account.h"
+
+class SavingsAccount : public Account{
+    private:
+        bool status;
+    public:
+        SavingsAccount(int accNum, double bal, double intRate, double annServCharge, bool AccStatus);
+};
+
+SavingsAccount::SavingsAccount(int SAccNum = 0, double SBal = 0.0, double SIntRate = 0.0, double SAnnServCharge = 0.0, bool AccStatus = true) : 
+Account(SAccNum, SBal, SIntRate, SAnnServCharge){
+    status =  AccStatus;
+}
+
+#endif
