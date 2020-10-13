c8a9e3814538681354c00e6f287008b300470873
jaronritter01 <61990047+jaronritter01@users.noreply.github.com>
2020-10-13 18:22:32 -0500

    This adds the time stamp of when the account object was created and some functions to go with it. Also changed the main file a little

--- account.h
+++ account.h
@@ -2,6 +2,7 @@
 #define ACCOUNT
 
 #include <string>
+#include <time.h>
 //make acc num a string
 
 using namespace std;
@@ -12,6 +13,8 @@ class Account{
         double balance;
         double interestRate;
         double annualServiceCharge;
+        tm *timeCreated;
+        
     public:
         Account(string accNum, double bal, double intRate, double annServCharge);
         virtual bool deposit(double dAmount);
@@ -24,6 +27,10 @@ class Account{
         double getInterestRate() const;
         double getAnnualServiceCharge() const;
         virtual void closeAcc();
+        int getMonthCreated() const;
+        int getDayCreated() const;
+        int getYearCreated() const;
+        string getDateCreated() const;
 };
 
 Account::Account(string accNum="0", double bal=0.0, double intRate=0.0, double annServCharge=0.0){
@@ -32,6 +39,32 @@ Account::Account(string accNum="0", double bal=0.0, double intRate=0.0, double a
     balance = bal;
     interestRate = intRate;
     annualServiceCharge = annServCharge;
+
+    time_t currentTime;
+    time(&currentTime);
+    timeCreated = localtime(&currentTime);
+}
+
+string Account::getDateCreated() const{
+    string dateLine, day, month, year;
+    day = to_string(timeCreated->tm_mday);
+    month = to_string(timeCreated->tm_mon + 1);
+    year = to_string(timeCreated->tm_year + 1900);
+    dateLine = month + ":" + day + ":" + year;
+    
+    return dateLine;
+}
+
+int Account::getYearCreated() const{
+    return timeCreated->tm_year + 1900;
+}
+
+int Account::getDayCreated() const{
+    return timeCreated->tm_mday;
+}
+
+int Account::getMonthCreated() const{
+    return timeCreated->tm_mon + 1;
 }
 
 void Account::setAnnualServiceCharge(double rate){
